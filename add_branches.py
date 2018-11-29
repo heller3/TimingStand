0af631347b1b12e793bda4b0cdf9d7c78f2e11ac
Ana Ovcharova <akovcharova@gmail.com>
2018-11-28 23:41:09 -0600

    Added run number branch and renamed macro

--- add_branches.py
+++ add_branches.py
@@ -5,7 +5,7 @@ from ROOT import *
 from array import array
 from collections import namedtuple
 
-def process_file(outfile, x0, y0, chidx, ptkidx):
+def process_file(outfile, run, x0, y0, chidx, ptkidx):
 
     rootfile = TFile(outfile, "UPDATE")
     if (rootfile.IsZombie() or not rootfile.IsOpen()):
@@ -21,18 +21,20 @@ def process_file(outfile, x0, y0, chidx, ptkidx):
         this_chidx = [-1 if x<99 else x-100 for x in chidx]
         this_ptkidx = [-1 if x<99 else x-100 for x in ptkidx]
 
-    arr_x0, arr_y0 = array('f',[x0]), array('f',[y0])
+    arr_x0, arr_y0, arr_run = array('f',[x0]), array('f',[y0]), array('i',[run])
     arr_chidx = array('i', this_chidx)
     arr_ptkidx = array('i', this_ptkidx)
 
     b_x0 = pulse.Branch("x0",arr_x0,"x0/F")
     b_y0 = pulse.Branch("y0",arr_y0,"y0/F")
+    b_run = pulse.Branch("run",arr_run,"run/I")
     b_chidx = pulse.Branch("chidx",arr_chidx,'chidx[{0}]/I'.format(len(chidx)))
     b_ptkidx = pulse.Branch("ptkidx",arr_ptkidx,'ptkidx[{0}]/I'.format(len(ptkidx)))
 
     for i in range(pulse.GetEntries()):
         b_x0.Fill()
         b_y0.Fill()
+        b_run.Fill()
         b_chidx.Fill()
         b_ptkidx.Fill()
     
@@ -86,7 +88,6 @@ if __name__ == '__main__':
     nchans = 16
     verbose = False
 
-    # vme_reco_dir = '/Users/ana/mtd/testbeam/data/VME/RECO/v6/'
     vme_reco_dir = '/eos/uscms/store/group/cmstestbeam/BTL_ETL/2018_11/data/VME/RECO/v6/'
     vme_file = vme_reco_dir+'DataVMETiming_RunRN.root'
 
@@ -107,4 +108,11 @@ if __name__ == '__main__':
                     continue
                 shutil.copyfile(infile, outfile)
                 print 'Processing file:', infile,
-                print process_file(outfile, ib.x0, ib.y0, ib.chidx, ib.ptkidx)
+                print process_file(outfile, run, ib.x0, ib.y0, ib.chidx, ib.ptkidx)
+
+    # run = 1660
+    # infile = '/Users/ana/mtd/testbeam/data/VME/RECO/v6/DataVMETiming_RunRN.root'.replace('RN',str(run))
+    # print 'Processing file:', infile,
+    # outfile = infile.replace('.root','_{}_bv{}.root'.format(dut_name, 500))
+    # shutil.copyfile(infile, outfile)
+    # print process_file(outfile, run, 10, 10.5, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
